bc8b40f5386baf5533e360aacee6ef9946b78ca7
Erkanovich <erinik90@gmail.com>
2021-05-17 13:51:38 +0200

    trolleri

---
+++ ClientApp/src/components/InfoCard.js
@@ -0,0 +1,24 @@
+﻿import React, { Component } from 'react';
+import InfoLogo from '../images/info-circle.svg';
+import './InfoCard.css';
+
+export default class InfoCard extends Component {
+  static displayName = InfoCard.name;
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <img className="card-img-top info_card_image" src={InfoLogo} alt="Infologo bild" />
+        <div className="card-body">
+          <p className="card-text">
+            {this.props.infoText}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
