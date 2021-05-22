8cceafdab707e42ac537082205313e7eca887dfb
PayamS <72343921+Payam-Shaker@users.noreply.github.com>
2021-05-22 15:28:31 +0200

    login & Register separated based on new design

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -5,6 +5,7 @@ import { Home } from './components/Home';
 import { Login } from './components/Login';
 import { AboutUs } from './components/AboutUs';
 import { AddCar } from './components/AddCar';
+import { Signup } from './components/Signup';
 
 import './custom.css'
 
@@ -18,6 +19,10 @@ export default class App extends Component {
         <Route path='/Login' component={Login} />
         <Route path='/AboutUs' component={AboutUs} />
         <Route path='/AddCar' component={AddCar} />
+        <Route path='/fetch-data' component={FetchData} />
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={Signup} />
+
       </Layout>
     );
   }
--- User.cs
+++ User.cs
@@ -7,6 +7,8 @@ namespace CarRental
 {
     public class User
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
